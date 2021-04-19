83ca0e799fcf22c43a410d76ad423afc2b0d229f
mackv42 <mackv42@gmail.com>
2021-04-19 09:16:35 -0500

    add chess

---
+++ chess/auth.js
@@ -0,0 +1,76 @@
+const endpoint = "javascript-chess-7p9ri.ondigitalocean.app/api/users/";
+const signInPath = "signin";
+const signUpPath = "signup";
+const verifyPath = "verify";
+
+var signInData = {
+	"token": ""
+}
+
+let signUp = document.getElementById("signUpSbmt");
+let signIn = document.getElementById("signInSbmt");
+
+let signUpModal = document.getElementById("signUpModal");
+let signInModal = document.getElementById("signInModal");
+
+signUp.onclick = function(evt){
+	let data = {
+		"email": document.getElementById("signupEmail").value,
+		"username": document.getElementById("signupUsername").value,
+		"password": document.getElementById("signupPassword").value
+	};
+
+	for(var val in Object.values(data)){
+		if(!data|| data.length < 1){
+			return;
+		}
+	}
+
+	axios.post("http://"+endpoint+"signup", data).then( function(response){
+		console.log(response);
+		$("#signUpModal").modal('hide');
+	});
+}
+
+signIn.onclick = function(evt){
+	let data = {
+		"email": document.getElementById("signinEmail").value,
+		"password": document.getElementById("signinPassword").value
+	}
+
+	for(var val in Object.values(data)){
+		if(!data|| data.length < 1){
+			return;
+		}
+	}
+
+	axios.post("http://"+endpoint+"signin", data).then( function(response){
+		if(response.data.success){
+			signInData.token = response.data.token;
+			axios.get("http://javascript-chess-7p9ri.ondigitalocean.app/requireauth/getGame?token="+signInData.token).then( function(response){
+				if(response.data.success){
+					currentBoard = response.data.board;
+					currentBoard.playerColor = response.data.playerColor;
+					RenderBoard(response.data.board);
+
+					register(signInData.token)
+				}
+			});
+			
+			$("#signInModal").modal('hide');
+			document.getElementById("ChessBoard").style.display = "block";
+
+			//GET current chess game
+			// Render the chess board
+			//  Attach Events
+		} else{
+			//Notify Error
+		}
+	});
+}
+
+function verify(token){
+	axios.get("http://"+endpoint+"verify?"+token).then( function( response ){
+		console.log("Verified");
+	});
+}
---
+++ chess/websockets.js
@@ -0,0 +1,20 @@
+let sockData = {
+	"wsUrl": "http://javascript-chess-7p9ri.ondigitalocean.app",
+}
+
+sockData.socket = io.connect(sockData.wsUrl)
+function register(token){
+	sockData.socket.emit('register', signInData.token);
+	sockData.token = signInData.token;
+	sockData.socket.on('makemove', function(){
+		console.log("emit");
+		axios.get("http://javascript-chess-7p9ri.ondigitalocean.app/requireauth/getGame?token="+sockData.token).then( function(response){
+			if(response.data.success){
+				currentBoard = response.data.board;
+				currentBoard.playerColor = response.data.playerColor;
+				RenderBoard(response.data.board);
+			
+			}
+		});
+	});
+}
